1c49a69cfa417732a37a73ef71c2836170748870
Владислав <robert_trebor@list.ru>
2024-06-09 12:30:16 +0300

    Homework done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Task 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.printf("«Ф. И. О. сотрудника — %s».%n", fullName);
+
+        System.out.println("Task 2");
+        System.out.printf("«Данные Ф. И. О. сотрудника для заполнения отчета — %s».%n", fullName.toUpperCase());
+
+        System.out.println("Task 3");
+        fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.printf("«Данные Ф. И. О. сотрудника — %s».%n", fullName);
+
     }
 }
